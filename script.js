d094005171cf280d373142fc9c2da68f7f17eb8a
aslanmarketolovich <157589424+aslanmarketolovich@users.noreply.github.com>
2024-03-05 19:19:11 +0500

    t

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function devide() {
     let b = document.getElementsByClassName("b")[0].value;
     let result = document.getElementsByClassName("result")[0];
     
-    if (a == 0){
+    if (b == 0){
         alert("На ноль делить нельзя")
     }
     let c = Number(a) / Number(b);
