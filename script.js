9d543c7c1d7ff8a252fc62d1dbb7a072ebc91a2b
aslanmarketolovich <157589424+aslanmarketolovich@users.noreply.github.com>
2024-03-05 19:04:08 +0500

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function pluse() {
+    let a = document.getElementsByClassName("a")[0].value;
+    let b = document.getElementsByClassName("b")[0].value;
+    let result = document.getElementsByClassName("result")[0];
+    let c = Number(a) + Number (b);
+    return result.value = c;
+}
+
+function minuse() {
+    let a = document.getElementsByClassName("a")[0].value;
+    let b = document.getElementsByClassName("b")[0].value;
+    let result = document.getElementsByClassName("result")[0];
+    let c = Number(a) - Number(b);
+    return result.value = c
+}
+
+function multiply() {
+    let a = document.getElementsByClassName("a")[0].value;
+    let b = document.getElementsByClassName("b")[0].value;
+    let result = document.getElementsByClassName("result")[0];
+    let c = Number(a) * Number(b);
+    return result.value = c
+}
+
+function devide() {
+    let a = document.getElementsByClassName("a")[0].value;
+    let b = document.getElementsByClassName("b")[0].value;
+    let result = document.getElementsByClassName("result")[0];
+    
+    if (a == 0){
+        alert("На ноль делить нельзя")
+    }
+    let c = Number(a) / Number(b);
+    return result.value = c
+
+}
